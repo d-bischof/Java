32c881b3658047b23d9369255cffe9cefce2dd87
Diego E Bischoff <dbischof@student.scf.edu>
2026-04-21 11:55:33 -0400

    elleven

---
+++ eleven/solution.java
@@ -0,0 +1,13 @@
+void main() {
+
+    var delta = new Triangle(3, 4, 5);
+
+    delta.setColor("red");
+    delta.getColor();
+    delta.getArea();
+    delta.getPerimeter();
+    delta.setFilled(true);
+    delta.getDateCreated();
+    System.out.println(delta);
+
+}
