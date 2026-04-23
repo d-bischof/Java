a131a61c7355dcda44d869c7f4a3fc33fdacd337
Diego E Bischoff <dbischof@student.scf.edu>
2026-04-23 11:52:32 -0400

    assignment 12

---
+++ twelve/ArrayBounds.java
@@ -0,0 +1,37 @@
+/**
+ * ArrayBounds.java
+ * COP 2250 - Assignment 12
+ * Exception Handling (Liang 12.3)
+ *
+ * Name:
+ * Date:
+ */
+import java.util.Scanner;
+
+public class ArrayBounds {
+
+    public static void main(String[] args) {
+        var input = new Scanner(System.in);
+
+        int data[] = {1, 2, 3, 4, 5};
+
+        for(;;) {
+            System.out.printf("please enter an integer (-1 or non-integer to quit): ");
+
+            int num = input.hasNextInt() ? input.nextInt() : -1;
+
+            try {
+                System.out.printf("data[%d] = %d\n", num, data[num]);
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.printf("Error: %s\n", e.getMessage());
+            } 
+
+            if (num == -1) break;
+
+        }
+
+        System.out.println("Goodbye.");
+        input.close();
+    }
+}
